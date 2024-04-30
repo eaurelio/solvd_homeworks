8d891b458de45d277f16f2ea53bb46780bb4be62
edson <edson.exe@outlook.com>
2024-04-30 09:16:17 -0300

    fixing task4

--- homework5/task4.js
+++ homework5/task4.js
@@ -1,13 +1,16 @@
 function getArrayIntersection(array1, array2) {
-  return array1.filter(value => array2.includes(value));
+  const set1 = new Set(array1);
+  const set2 = new Set(array2);
+  const intersection = [...set1].filter(value => set2.has(value));
+  return intersection;
 }
 
 function getArrayUnion(array1, array2) {
   return [...new Set([...array1, ...array2])];
 }
 
-const array1 = [1, 2, 3, 4, 5];
-const array2 = [3, 4, 5, 6, 7];
+const array1 = [1, 2, 3, 3, 4, 5, 'a', 'b'];
+const array2 = ['b', 'c',3, 3, 4, 4, 5, 6, 7];
 
 const intersection = getArrayIntersection(array1, array2);
 console.log('Intersection:', intersection);
